b8d0bfd75e7c3d93bc0de3dd6c300c05e3dcf25d
Dr. J <brownlowj@yahoo.com>
2024-04-05 14:07:03 -0700

    Add files via upload

---
+++ BrLw.R
@@ -0,0 +1,19 @@
+BrLw <-function(numClass,odds,countoDo){
+
+days=1:365
+count=countoDo
+repeatn=numClass
+varstor=numeric()
+numWins=0
+  brownlow = 0
+  for (i in 1:countoDo) {
+    sam=sample(days,size=numClass,replace = T)
+    y=duplicated(sam)
+    if (sum(y)>0) {
+      brownlow=brownlow+1
+    }
+    else brownlow = brownlow - odds
+    
+  }
+ return(brownlow)
+}
